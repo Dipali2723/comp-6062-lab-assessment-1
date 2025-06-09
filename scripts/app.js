3cdf35c15d83473d340f85980736254602416083
Dipali276 <dipalighogharica@gmail.com>
2025-06-09 19:59:39 -0400

    add classlist

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 console.log('JS loaded!!')
 const myFullName = 'Dipali J Ghoghari';
 const myStudentNumber = 1228649;
-const result= 'myFullName'+'myStudentNumber';
-console.log(result);
+const result= '${myFullName} - ${myStudentNumber}';
+
+const primaryHeadingContent=document.querySelector('h1');
+
+primaryHeadingContent.innerHTML='h1';
+
+element.classList.add(primaryHeadingContent);
+
+
+
+
+
+
+
