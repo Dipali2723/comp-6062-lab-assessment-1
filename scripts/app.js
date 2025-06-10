f8c8cfede990f3a25c5fa3cd82f2c01429529fc5
Dipali276 <dipalighogharica@gmail.com>
2025-06-09 20:12:21 -0400

    syntax error

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 console.log('JS loaded!!')
 const myFullName = 'Dipali J Ghoghari';
 const myStudentNumber = 1228649;
-const result= '${myFullName} - ${myStudentNumber}';
+const result= `${myFullName} - ${myStudentNumber}`;
 
 const primaryHeadingContent=document.querySelector('h1');
 
-primaryHeadingContent.innerHTML='${myFullName} - ${myStudentNumber}';
+primaryHeadingContent.innerHTML=`${myFullName} - ${myStudentNumber}`;
 
 primaryHeadingContent.classList.add(primaryHeadingContent);
 
