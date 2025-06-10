f55b9ede687dd04ed52ed6a311348aec0687aa7a
Dipali276 <dipalighogharica@gmail.com>
2025-06-09 20:10:37 -0400

    h1

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ const myFullName = 'Dipali J Ghoghari';
 const myStudentNumber = 1228649;
 const result= '${myFullName} - ${myStudentNumber}';
 
-const primaryHeadingContent=document.querySelector('result');
+const primaryHeadingContent=document.querySelector('h1');
 
 primaryHeadingContent.innerHTML='${myFullName} - ${myStudentNumber}';
 
